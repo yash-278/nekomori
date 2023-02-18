76c2403d1a1e5192bce3db0291ef5f7db2ff6d68
Yash Kadam <yashkadam278@gmail.com>
2023-02-18 12:02:15 +0530

    add Tanstack Query, TailwindCSS, React Router

---
+++ tailwind.comfig.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {},
+    },
+    fontFamily: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio")],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: "build",
+  },
+
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        svgoConfig: {
+          plugins: [
+            {
+              removeViewBox: false,
+            },
+          ],
+        },
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+});
